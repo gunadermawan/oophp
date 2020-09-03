f152c48840381175f71ab834a714e75b5f415428
gunaderamwan <gunadermawan62@gmail.com>
2020-09-03 13:28:40 +0700

    menambahkan sampel dari prperty dan method

--- produk.php
+++ produk.php
@@ -1,7 +1,43 @@
 <?php 
     class Produk {
+        // property adalah representasi data dari sebuah object atau
+        // variabel dalam object
+        // ditambah visibility didepanya
 
+        // method adalah representasi perilaku dari sebuah object atau
+        // function dalam object
+        // ditambah visibility di depanya
+
+
+        // dibawah ini membuat property
+        public  $harga = 0,
+                $prosesor = "intel i7'10th Gen",
+                $sistemOperasi = "Linux";
+
+        // dibawah pembuatan method atau function dalam kelas
+        public function getLabel() {
+            // $this digunakan untuk ngambil property dluar function tapi msaih didalam
+            // class yang sama,kalo ngga ada $this nanti membuat variabel baru didalam 
+            // function itu sendiri
+            return "$this->harga,
+                    $this->prosesor,
+                    $this->sistemOperasi";
+        }
     }
+    // dibawah pemanggilan property
     $laptop = new Produk();
-    $Komputer = new Produk();
+    $laptop->harga = 1000000;
+    $laptop->prosesor ="AMD Ryzen 5 3th Gen";
+    $laptop->sistemOperasi;
+    
+
+    $komputer = new Produk();
+    $komputer->harga = 12000000;
+    $komputer->prosesor = "Intel i3 8th Gen";
+    $komputer->sistemOperasi = "Windows 10" ;
+
+    // dibwah pmaggilan method
+    echo "Laptop dengan spesifikasi : " . $laptop->getLabel();
+    echo "<br>";
+    echo "komputer dengan spesifikasi : " . $komputer->getLabel();
 ?>
